43bf87879e9c7f1e51462febffac6a6761b44fe1
majidrouhi <mrouhi13@icloud.com>
2017-08-10 16:08:25 +0430

    fix formula issues.

--- private/controllers/answer.php
+++ private/controllers/answer.php
@@ -75,7 +75,7 @@
 				foreach ($dataset as $data)
 				{
 					$point = self::getPoint($answer['answer'], $data['answer']);
-					$weight = $point * $questions[$answer['question_id'] - 1]['weight'];
+					$weight = $point * 1;
 					$weights[$data['user_id']][$answer['question_id']] = $weight;
 				}
 			}
@@ -88,18 +88,6 @@
 			$maxWeight = array_sum($_data[$_userId]);
 			$maxCount = count($_data[$_userId]);
 
-			// $maxWeight = 0;
-			// $maxCount = 0;
-
-			// foreach ($_data as $user)
-			// {
-			// 	$sum = array_sum($user);
-			// 	$count = count($user);
-
-			// 	if ($sum > $maxWeight) $maxWeight = $sum;
-			// 	if ($count > $maxCount) $maxCount = $count;
-			// }
-
 			foreach($_data as $userId => $w)
 			{
 				$name = $this -> user -> get($userId)['first_name'];
@@ -107,10 +95,9 @@
 
 				$answerWeight = array_sum($w);
 				$similarCount = count($w);
-				$questionWeight = $similarCount / $maxCount;
-				$answerPercent = ($answerWeight * 100) / $maxWeight;
-				$questionPercent = ($questionWeight * 100) / 1;
-				$totalPercent = round((($answerPercent + $questionPercent) * 100) / 200, 2);
+				$answerPercent = ($answerWeight * 200) / ($maxWeight + ($totalQuestions * 1));
+				$questionPercent = ($similarCount * 200) / ($maxCount + $totalQuestions);
+				$totalPercent = round((($answerPercent * $similarCount) + $questionPercent) / ($similarCount + 1) , 2);
 
 				if ($userId != $_userId) $info[] = ['name' => $name, 'answerWeight' => $answerWeight, 'answerPercent' => $answerPercent, 'questionWeight' => $questionWeight, 'questionPercent' => $questionPercent, 'totalPercent' => $totalPercent, 'totalQuestions' => $totalQuestions, 'similarCount' => $similarCount];
 			}
@@ -121,11 +108,11 @@
 		private static function getPoint ($_sample, $_answer)
 		{
 			$statusSet = [
-				1 => [1 => 1, 2 => 0.75, 3 => 0.5, 4 => 0.25, 5 => 0],
-				2 => [1 => 0.75, 2 => 1, 3 => 0.62, 4 => 0.25, 5 => 0],
-				3 => [1 => 0.5, 2 => 0.62, 3 => 1, 4 => 0.62, 5 => 0.5],
-				4 => [1 => 0.25, 2 => 0.25, 3 => 0.62, 4 => 1, 5 => 0.75],
-				5 => [1 => 0, 2 => 0, 3 => 0.5, 4 => 0.75, 5 => 1]
+				1 => [1 => 1, 2 => 0.875, 3 => 0.25, 4 => 0.125, 5 => 0],
+				2 => [1 => 0.875, 2 => 1, 3 => 0.5, 4 => 0.25, 5 => 0.125],
+				3 => [1 => 0.25, 2 => 0.5, 3 => 1, 4 => 0.5, 5 => 0.25],
+				4 => [1 => 0.125, 2 => 0.25, 3 => 0.50, 4 => 1, 5 => 0.875],
+				5 => [1 => 0, 2 => 0.125, 3 => 0.25, 4 => 0.875, 5 => 1]
 			];
 
 			return $statusSet[$_sample][$_answer];
